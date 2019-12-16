c5edca65acb02ac33376829a4bca84f7bb17422d
mazores <m_azores91446@fanshaweonline.ca>
2019-12-16 10:27:01 -0500

    Adding in config js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 const config = {
-    host: "localhost",
-    port: 8889, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_portfolio"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    uname: "b7fe1fe62a6c69", // 8889 for mac
+    pword: "e072c11", // leave blank for windows
+    database: "heroku_d59f0a274b7af67"
+
+    // host: "localhost",
+    // port: 8889, // 8889 for mac
+    // uname: "root",
+    // pword: "root", // leave blank for windows
+    // database: "db_portfolio"
 }
 
 module.exports = config;
